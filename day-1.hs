a43c8ee577f69ee3576092949a49be28fec43abe
Marco Munizaga <git@marcopolo.io>
2020-12-06 20:54:45 -0800

    Rename Day 0 -> Day 1

--- day-1.hs
+++ day-1.hs
@@ -5,16 +5,16 @@ import Data.Maybe
 targetNum = 2020
 
 main = do
-  content <- readFile "inputs/day-0-0"
+  content <- readFile "inputs/day-1"
   let numbers = sort $ map read $ lines content :: [Int]
       revNumbers = reverse numbers
       magicPair = findMatchingNumber numbers revNumbers
       magicPair2 = fromMaybe (0, 0, 0) $ findMatchingNumberPart2 numbers
    in putStrLn $ printAnswer magicPair ++ "\n\n" ++ printAnswer2 magicPair2
 
-printAnswer (a, b) = "Magic Pair: " ++ show (a, b) ++ "\n" ++ "Answer: " ++ show (a * b)
+printAnswer (a, b) = "Part 1: Magic Pair: " ++ show (a, b) ++ "\n" ++ "Answer: " ++ show (a * b)
 
-printAnswer2 (a, b, c) = "Magic Pair: " ++ show (a, b, c) ++ "\n" ++ "Answer: " ++ show (a * b * c)
+printAnswer2 (a, b, c) = "Part 2: Magic Pair: " ++ show (a, b, c) ++ "\n" ++ "Answer: " ++ show (a * b * c)
 
 findMatchingNumber :: [Int] -> [Int] -> (Int, Int)
 findMatchingNumber (smallNumber : numbers) (bigNumber : revNumbers)
